631c263ec6b90ea5ae8b945a28afb015f0f05ca0
Zachary Ironside <zironside@hotmail.com>
2017-08-10 03:11:19 +0100

    add stoping to watch

--- main.c
+++ main.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <curses.h>
 
 int main(int argc, char *argv[]) {
     int curTime = 0;
     int startTime = time(NULL);
+    WINDOW *w;
+    char c;
+
+    int breakTime;
+    bool stop = false;
     for (;;) {
         int seconds = curTime % 60;
         int minute =  (curTime / 60) % 60;
         int hour = curTime / (60*60);
-        sleep(1);
+
+
+        w = initscr();
+        timeout(1);
+        c = getch();
+        endwin();
+        if(c == '\n') {
+            stop = (stop)? false : true;
+            if(!stop){
+                stop = false;
+                breakTime = time(NULL) - (curTime+startTime);
+                startTime += breakTime;
+            }
+        }
         printf("%02d:%02d:%02d \r", hour, minute, seconds);
         fflush(stdout);
-        curTime = time(NULL) - startTime;
+        if(!stop) {
+            curTime = time(NULL) - startTime;
+        }
     }
     return 0;
 }
